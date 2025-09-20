80f536194b20b36118a7d0d634f9a633491c1237
Kishan Patel <kbpatel309@gmail.com>
2025-09-20 04:20:09 +0000

    base app

--- public-holidays-app/src/index.js
+++ public-holidays-app/src/index.js
@@ -2,16 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import 'papercss/dist/paper.min.css';
+
+// Create a single QueryClient instance (manage all queries)
+const QueryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  // Wrap the app with QueryClientProvider so React Query can be used anywhere
+  <QueryClientProvider client={queryClient}>
     <App />
-  </React.StrictMode>
+  </QueryClientProvider>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
